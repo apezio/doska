5aef304eda9c5ccf01d34626fdc1baa0a78baa99
Rita Romenkova <rita.romenkova@gmail.com>
2026-08-12 00:12:43 +0300

    feat: download button for landing

--- apps/landing/src/links.ts
+++ apps/landing/src/links.ts
@@ -7,4 +7,6 @@ export const app = `${appOrigin}/d/welcome`
 export const author = "https://github.com/romenkova"
 export const repo = `${author}/doska`
 export const releases = `${repo}/releases`
+export const releasesLatest = `${releases}/latest`
+export const repoApi = "https://api.github.com/repos/romenkova/doska"
 export const roadmap = "https://app.doska.sh/p/2af2848df270cb5b8a4e73e7a362b19b"

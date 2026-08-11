1af368898d2dceda4182ff430d084e1af49bf79c
Rita Romenkova <rita.romenkova@gmail.com>
2026-08-12 00:58:32 +0300

    ci: remove cargo cache

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -22,7 +22,6 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
-  workers: process.env.CI ? "100%" : undefined,
   reporter: "list",
   use: {
     baseURL: "http://localhost:4173",
